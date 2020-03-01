d0c62e569bb7d5bc0d74ff4df7c4f158ba42eb7d
nahon1 <60366732+nahon1@users.noreply.github.com>
2020-02-29 17:15:08 -0800

    edits
    complete

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -1,6 +1,15 @@
 extends StaticBody2D
 
 var points = 10
+onready var _target = position
+
+onready var t = $Timer
 
 func _ready():
-	pass
+	position.y = -66
+	var time = rand_range(0, 2.0)
+	t.set_wait_time(time)
+	t.start()
+	yield(t, "timeout")
+	$Tween.interpolate_property(self, "position", position, _target, 1.6, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	$Tween.start()
